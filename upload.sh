490f934ff066af85e54a266ba947d2cc98a1ae1e
Leo Chen <chenlt78@qq.com>
2017-12-14 00:39:37 +0800

    Adding cron upload job
    1. update upload.sh to run in cron
    2. cron list: 0 6 * * * /data/s3-static-video-website/upload.sh >> /var/log/s3-reinvent-upload.log
    
    Signed-off-by: Leo Chen <chenlt78@qq.com>

--- upload.sh
+++ upload.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+echo "Start Time:`date`"
 
+pushd /data/s3-static-video-website
 
 ./upload_chinakb_2015.sh
 ./upload_chinakb_2016.sh
 ./upload_chinakb_2017.sh
+
+
+popd
+
+echo "End Time:`date`"
+
+exit 0
